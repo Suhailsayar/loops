4ef02f5cce2b7044c4bb897fd60854694485c140
Suhailsayar <suhailsahil508@gmail.com>
2025-08-12 17:23:28 +0530

    leet code trending loop problem

---
+++ loops11.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int main() {  
+  int count = 0;
+  int i = 1;
+  int j = 1;
+  int k = 1;
+  int n = 10;
+  
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j <= i; j++) {
+      for (k = 1; k <= n * n * n; k++) {
+        printf("hello world\n");
+        count++;
+      }
+    }
+  }
+  
+  printf("%d\n", count);
+  return 0;
+}
